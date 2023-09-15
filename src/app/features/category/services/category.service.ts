5101b2296367bb8772e59f844d046d10709b8a50
rothblaa <adam.j.rothblatt@outlook.com>
2023-09-15 12:52:41 -0500

    service, model, and environment

--- src/app/features/category/services/category.service.ts
+++ src/app/features/category/services/category.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Category } from '../models/category.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +14,11 @@ export class CategoryService {
 
   //takes model and returns observable
   addCategory(model: AddCategoryRequest): Observable<void> {
-    return this.http.post<void>('https://localhost:7196/api/categories', model);
+    return this.http.post<void>(`${environment.apiBaseUrl}/api/categories`, model);
+  }
+
+  // returns obs of type categoriesList
+  getAllCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/Categories`);
   }
 }
