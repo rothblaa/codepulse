91aef253696f64e271bf19986fa8575727ac9265
rothblaa <adam.j.rothblatt@outlook.com>
2023-09-15 13:34:06 -0500

    category list changes

--- src/app/features/category/category-list/category-list.component.ts
+++ src/app/features/category/category-list/category-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../services/category.service';
+import { Observable } from 'rxjs';
 import { Category } from '../models/category.model';
 
 @Component({
@@ -9,18 +10,14 @@ import { Category } from '../models/category.model';
 })
 export class CategoryListComponent implements OnInit {
 
-  categories?: Category[];
+  categories$?: Observable<Category[]>
 
   constructor(private categoryService: CategoryService) {
 
   }
 
   ngOnInit(): void {
-    this.categoryService.getAllCategories()
-    .subscribe({
-      next: (response) => {
-        this.categories = response;
-      }
-    });
+    this.categories$ = this.categoryService.getAllCategories();
+
   }
 }
