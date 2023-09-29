49e5f994499de94b4aa7f81e0e042388ccd0d091
rothblaa <adam.j.rothblatt@outlook.com>
2023-09-29 15:30:30 -0500

    Blog Post API Get and Post blogposts

---
+++ CodePulse.API/Repositories/Interface/IBlogPostRepository.cs
@@ -0,0 +1,11 @@
+﻿using CodePulse.API.Models.Domain;
+using System.Collections;
+
+namespace CodePulse.API.Repositories.Interface
+{
+    public interface IBlogPostRepository
+    {
+        Task<BlogPost> CreateAsync(BlogPost blogPost);
+        Task<IEnumerable<BlogPost>> GetAllAsync();
+    }
+}
