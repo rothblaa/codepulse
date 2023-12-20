a957507bf290cc036ea68d58d1eda3f7a912e164
rothblaa <adam.j.rothblatt@outlook.com>
2023-12-19 21:53:05 -0600

    commit to save current progress

--- src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
+++ src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -18,6 +18,7 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
   model?: BlogPost;
   categories$?: Observable<Category[]>;
   selectedCategories?: string[];
+  isImageSelectorVisible: boolean = false;
 
   routeSubscription?: Subscription;
   updateBlogPostSubscription?: Subscription;
@@ -88,6 +89,14 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
         }
       })
     }
+  }
+
+  openImageSelector(): void {
+    this.isImageSelectorVisible = true;
+  }
+
+  closeImageSelector(): void {
+    this.isImageSelectorVisible = false;
   }
 
   ngOnDestroy(): void {
