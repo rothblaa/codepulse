8701cec39dccb6742daeb12e5a3b26c4e919f919
rothblaa <adam.j.rothblatt@outlook.com>
2024-01-18 17:47:34 -0600

    imageselector service and get/post

--- src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
+++ src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -6,6 +6,7 @@ import { BlogPost } from '../models/blog-post.model';
 import { CategoryService } from '../../category/services/category.service';
 import { Category } from '../../category/models/category.model';
 import { UpdateBlogPost } from '../models/update-blog-post';
+import { ImageService } from 'src/app/shared/components/image-selector/image.service';
 
 @Component({
   selector: 'app-edit-blogpost',
@@ -24,12 +25,14 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
   updateBlogPostSubscription?: Subscription;
   getBlogPostSubscription?: Subscription;
   deleteBlogPostSubscription?: Subscription;
+  imageSelectSubscription?: Subscription;
 
 
   constructor(private route: ActivatedRoute,
     private blogPostService: BlogPostService,
     private categoryService: CategoryService,
-    private router: Router) {
+    private router: Router,
+    private imageService: ImageService) {
 
     }
 
@@ -51,6 +54,16 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
             }
           });
         }
+
+        this.imageSelectSubscription = this.imageService.onSelectImage()
+        .subscribe({
+          next: (response) => {
+            if (this.model) {
+              this.model.featuredImageUrl = response.url;
+              this.isImageSelectorVisible = false;
+            }
+          }
+        })
       }
     })
   }
@@ -104,6 +117,7 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
     this.updateBlogPostSubscription?.unsubscribe();
     this.getBlogPostSubscription?.unsubscribe();
     this.deleteBlogPostSubscription?.unsubscribe();
+    this.imageSelectSubscription?.unsubscribe();
   }
 
 }
--- src/app/shared/components/image-selector/image-selector.component.ts
+++ src/app/shared/components/image-selector/image-selector.component.ts
@@ -1,20 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ImageService } from './image.service';
+import { Observable } from 'rxjs';
+import { BlogImage } from '../../models/blog-image.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-image-selector',
   templateUrl: './image-selector.component.html',
   styleUrls: ['./image-selector.component.css']
 })
-export class ImageSelectorComponent {
+export class ImageSelectorComponent implements OnInit {
+
+  private file?: File;
+  fileName: string = '';
+  title: string = '';
+  images$?: Observable<BlogImage[]>;
+
+  @ViewChild('form', {static: false}) imageUploadForm?: NgForm;
 
   constructor(private imageService: ImageService) {
 
   }
 
-  private file?: File;
-  fileName: string = '';
-  title: string = '';
+  ngOnInit(): void {
+    this.getImages();
+  }
+
+
 
   onFileUploadChange(event: Event): void {
     const element = event.currentTarget as HTMLInputElement;
@@ -27,10 +39,19 @@ export class ImageSelectorComponent {
       this.imageService.uploadImage(this.file, this.fileName, this.title)
       .subscribe({
         next: (response) => {
-          console.log(response);
+          this.imageUploadForm?.resetForm();
+          this.getImages();
         }
       });
     }
+  }
+
+  selectImage(image: BlogImage): void {
+    this.imageService.selectImage(image);
+  }
+
+  private getImages() {
+    this.images$ = this.imageService.getAllImages();
   }
 
 }
