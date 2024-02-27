ac00716a5817b2af4e2bd6e3d04748d22a0ef7d0
rothblaa <adam.j.rothblatt@outlook.com>
2024-02-26 21:26:36 -0600

    create jwt token

--- CodePulse.API/Controllers/AuthController.cs
+++ CodePulse.API/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 ﻿using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -10,10 +11,13 @@ namespace CodePulse.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly UserManager<IdentityUser> userManager;
+        private readonly ITokenRepository tokenRepository;
 
-        public AuthController(UserManager<IdentityUser> userManager)
+        public AuthController(UserManager<IdentityUser> userManager,
+            ITokenRepository tokenRepository)
         {
             this.userManager = userManager;
+            this.tokenRepository = tokenRepository;
         }
 
         // POST: {apibaseurl}/api/auth/login
@@ -34,11 +38,13 @@ namespace CodePulse.API.Controllers
                     var roles = await userManager.GetRolesAsync(identityUser);
 
                     // Create a Token and Response
+                    var jwtToken = tokenRepository.CreateJwtToken(identityUser, roles.ToList());
+
                     var response = new LoginResponseDto()
                     {
                         Email = request.Email,
                         Roles = roles.ToList(),
-                        Token = "TOKEN"
+                        Token = jwtToken
                     };
 
                     return Ok(response);
