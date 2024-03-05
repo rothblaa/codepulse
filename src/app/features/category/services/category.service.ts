3f3c067ecb027405d1f91026be4cff8305d4bc15
rothblaa <adam.j.rothblatt@outlook.com>
2024-03-05 13:42:24 -0600

    pass JWT token with API calls

--- src/app/features/category/services/category.service.ts
+++ src/app/features/category/services/category.service.ts
@@ -5,18 +5,15 @@ import { HttpClient } from '@angular/common/http';
 import { Category } from '../models/category.model';
 import { environment } from 'src/environments/environment';
 import { UpdateCategoryRequest } from '../models/update-category-request.model';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor(private http: HttpClient) { }
-
-  //takes model and returns observable
-  addCategory(model: AddCategoryRequest): Observable<void> {
-    return this.http.post<void>(`${environment.apiBaseUrl}/api/categories`, model);
-  }
+  constructor(private http: HttpClient,
+    private cookieService: CookieService) { }
 
   // returns obs of type categoriesList
   getAllCategories(): Observable<Category[]> {
@@ -27,13 +24,30 @@ export class CategoryService {
     return this.http.get<Category>(`${environment.apiBaseUrl}/api/categories/${id}`)
   }
 
+  //takes model and returns observable
+  addCategory(model: AddCategoryRequest): Observable<void> {
+    return this.http.post<void>(`${environment.apiBaseUrl}/api/categories`, model, {
+      headers: {
+        'Authorization': this.cookieService.get('Authorization')
+      }
+    });
+  }
+
   updateCategory(id: string, UpdateCategoryRequest: UpdateCategoryRequest):
   Observable<Category> {
     return this.http.put<Category>(`${environment.apiBaseUrl}/api/categories/${id}`,
-    UpdateCategoryRequest);
+    UpdateCategoryRequest,{
+      headers: {
+        'Authorization': this.cookieService.get('Authorization')
+      }
+    });
   }
 
   deleteCategory(id: string): Observable<Category> {
-    return this.http.delete<Category>(`${environment.apiBaseUrl}/api/categories/${id}`)
+    return this.http.delete<Category>(`${environment.apiBaseUrl}/api/categories/${id}`, {
+      headers: {
+        'Authorization': this.cookieService.get('Authorization')
+      }
+    });
   }
 }
