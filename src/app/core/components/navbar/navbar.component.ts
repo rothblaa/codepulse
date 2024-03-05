53315d596534c87e6c365af23a72d8fb85b8b51b
rothblaa <adam.j.rothblatt@outlook.com>
2024-03-04 19:37:24 -0600

    Implement logout

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/features/auth/models/user.model';
 import { AuthService } from 'src/app/features/auth/services/auth.service';
 
 @Component({
@@ -8,20 +10,26 @@ import { AuthService } from 'src/app/features/auth/services/auth.service';
 })
 export class NavbarComponent implements OnInit {
 
-  /**
-   *
-   */
-  constructor(private authService: AuthService) {
+  user?: User;
 
+  constructor(private authService: AuthService,
+    private router: Router) {
   }
 
   ngOnInit(): void {
     this.authService.user()
     .subscribe({
       next: (response) => {
-        console.log(response);
+        this.user = response;
       }
-    })
+    });
+
+    this.user = this.authService.getUser();
+  }
+
+  onLogout(): void {
+    this.authService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
